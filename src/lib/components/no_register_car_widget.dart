7c195ce527a13037473e63348bf36d443eb8cf4e
Leandro Resende <resendeleandro565@gmail.com>
2025-05-18 11:17:00 +0100

    Added messages possibility between users and implemented push notifications for that messages

---
+++ src/lib/components/no_register_car_widget.dart
@@ -0,0 +1,66 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'no_register_car_model.dart';
+export 'no_register_car_model.dart';
+
+class NoRegisterCarWidget extends StatefulWidget {
+  const NoRegisterCarWidget({super.key});
+
+  @override
+  State<NoRegisterCarWidget> createState() => _NoRegisterCarWidgetState();
+}
+
+class _NoRegisterCarWidgetState extends State<NoRegisterCarWidget> {
+  late NoRegisterCarModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => NoRegisterCarModel());
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.sizeOf(context).width * 1.0,
+      height: MediaQuery.sizeOf(context).height * 1.0,
+      decoration: BoxDecoration(
+        color: Color(0xE9E9E9E9),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.directions_car_sharp,
+            color: Color(0xFF787878),
+            size: 38.0,
+          ),
+          Text(
+            'No registered car',
+            style: FlutterFlowTheme.of(context).bodyMedium.override(
+                  fontFamily: 'Inter',
+                  fontSize: 20.0,
+                  letterSpacing: 0.0,
+                  fontWeight: FontWeight.w200,
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ src/lib/custom_code/actions/index.dart
@@ -0,0 +1,7 @@
+export 'convert_to_uppercase.dart' show convertToUppercase;
+export 'remove_current_user_from_receivers.dart'
+    show removeCurrentUserFromReceivers;
+export 'delete_messages_with_no_receivers.dart'
+    show deleteMessagesWithNoReceivers;
+export 'delete_message_if_no_receivers.dart' show deleteMessageIfNoReceivers;
+export 'add_receivers_to_message.dart' show addReceiversToMessage;
